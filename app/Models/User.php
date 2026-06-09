be31d8efa7e4595fee867bdc909e49ac77f5895c
unknown <phantomfe98@gmail.com>
2026-06-09 15:17:23 +0700

    fix: replace Fillable attribute with traditional fillable array on User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,18 +4,24 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Attributes\Fillable;
 use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-#[Fillable(['name', 'email', 'password', 'foto', 'role'])]
 #[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'foto',
+        'role',
+    ];
 
     /**
      * Get the attributes that should be cast.
